a8c91e518383549c2513e87320a09f695ad63a7d
Chris Gross <cgross81@me.com>
2015-05-07 19:37:46 -0400

    Completed Activity Numbers
    Completed Week 1 Activity: Numbers

--- Week1/goal1_basics/js/main.js
+++ Week1/goal1_basics/js/main.js
@@ -110,35 +110,61 @@ console.log('------ strings ----------');
 console.log('------ numbers ----------');
 
     // arithmetic operators:  +, -, /, *, % (modulo)
-
+		
 
     //basic math
-
+		var counter = 10;
+		console.log ("basic math: ", counter + 1);
 
     //quick operations with "assignment operator" +=, -=, *=, /=, %=
     //one example below, can show more examples with other assignment operators
-
+		var num1 = 10, num2 = 5;
+		num1 += num2;
+		console.log ("+=: ", num1);
 
     //increment or decrement numbers by 1 with ++ or --
+		var myNum = 5;
+		console.log ("myNum: ", myNum);
+		
+		myNum++;
+		console.log ("myNum: ", myNum);
 
+		myNum--;
+		console.log ("myNum: ", myNum);		
+		
     //this decrements right before it is being used
+		var num = 10;
+		--num;
+		console.log ("--counter: ", num);
 
     //this decrements after it is used
-
+		//num--;
+		//console.log ("counter--: ", num);
+	
     //comment out the above example
     //this decrements after it is being used as well
+		console.log ("counter--: ", num--);
+		console.log ("counter--: ", num);
+
 
 
     //add a number to a string is always a string
-
+		var myStr = "6" + 2;
+		console.log ("Numbers + Strings: ", myStr);
 
     //other math operations will result in a number
-
+		var myStr = "6" / 2;
+		console.log ("Numbers / Strings: ", myStr);
 
     //order of operations: ( ), *, /, +, -
-
+		var num = 4 + (6 * 5 - 10) / 5;
+		console.log ("Order of Operations: ", num);
 
     //modulo example
+		var num1 = 10;
+		var num2 = 3;
+		console.log ("Remainder: ", num1%num2);
+		
 
 
 // arrays: (definition, indexing, getter/setter)
