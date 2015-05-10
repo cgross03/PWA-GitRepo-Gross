f27a81743f5c3eff9ca6b7cba873dbf0665ef2b5
Chris Gross <cgross81@me.com>
2015-05-09 22:35:38 -0400

    Completed Wk 1 Duel
    Completed Wk 1 Duel assignment.

--- Week1/goal1_assign_duel1/js/main.js
+++ Week1/goal1_assign_duel1/js/main.js
@@ -1,48 +1,90 @@
-// Chris Gross
-// PWA1 Online
-// 5-9-15
+/*
+Chris Gross
+Analyze Duel Week One
+Online
+5-9-15
+*/
 
+// Self executing function.
+(function(){
 
-(function(){  // Self Exicuting Function
-    
-    console.log ("Fight!!!"); // Checking for js file connection to html file.
-    
-    
-    //Player names.
-    var playerOneName = "Captain";
-    var playerTwoName = "Commander";
-        
-    // Player damage.
-    var playerOneDamage = 20;
-    var playerTwoDamage = 20;
-    
-    //Player health.
+    console.log("FIGHT!!!");
+
+    //player name
+    var playerOneName = "Spiderman";
+    var playerTwoName = "Batman";
+
+    //player damage
+    var player1Damage = 20;
+    var player2Damage = 20;
+
+    //player health
     var playerOneHealth = 100;
     var playerTwoHealth = 100;
-    
-    //Game round.
-    var round = 1;
-    
+
+    //initiate round
+    var round=0;
+
     function fight(){
-        console.log ("In the Fight Function");
-        
-        for (var i=0; i<10; i++){ // "for loop" to set a limit to 10 rounds. Continue the game until the 10th round. Then break.
-            console.log (i);
-        }; 
-        
+        alert(playerOneName+":"+playerOneHealth+"  *START*  "+playerTwoName+":"+playerTwoHealth);
+        // Begin loop. If the round is less than 10 continue to next round. If at 10, break.
+        for (var i = 0; i < 10; i++){
+            
+            // Player damage
+            var minDamage1 = player1Damage * .5;
+            var minDamage2 = player2Damage * .5;
+            
+            //Assigns random amount of damage to be dealt to each player.
+            var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1); 
+            var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
+
+            //Player health.
+            playerOneHealth-=f1;
+            playerTwoHealth-=f2;
+
+            console.log(playerOneName+": "+playerOneHealth + " " + playerTwoName+":"+playerTwoHealth);
+
+            //Check for winner
+            var result = winnerCheck();
+            //Console log the result
+            console.log(result);
+            
+            //If there is no winner display "no winner" and advance to next round.
+            if (result==="No Winner"){
+                round++;
+                alert(playerOneName+":"+playerOneHealth+"  *ROUND "+round+" OVER"+"*  "+playerTwoName+":"+playerTwoHealth);
+
+            }else{
+                // Display Alert box with results.
+                alert(result);
+                break;
+            };
+
+        };
     };
-    
+
     function winnerCheck(){
+        var result="no winner";
         
-       
+        // If both players drop to below 1 health point, display "You Both Die"
+        if (playerOneHealth<1 && playerTwoHealth<1){
+            result = "You Both Die!";
+            
+        //If player one health drops below 1, display "Player Two Wins"
+        }else if(playerOneHealth<1){
+            result =playerTwoName+" WINS!!!"
         
+        //If player two health drops below 1, display "Player One Wins"    
+        } else if (playerTwoHealth<1){
+            result = playerOneName+" WINS!!!"
+            
+        };
+        
+       return result;
+    
     };
-    
-    // *****Program starts after this line.*****
-    console.log ("Program Starts");
+
+    // *****Program Starts Below*****
     fight();
-    
-    
-    
-    
-    })(); 
+
+})();
